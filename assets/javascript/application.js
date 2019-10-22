a21d371b793e4b40aa09ec4ca63b09a54e271b2f
Joey Prijs <joey.prijs@gmail.com>
2019-10-22 13:21:25 +0200

    Seperate Tiny Slider styling (vendor and overrides), add touch/arrow keys/mouse drag support

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -2,9 +2,13 @@ var isOurWorkPage = document.getElementsByClassName('our-work');
 
 if (isOurWorkPage.length > 0) {
   var slider = tns({
-    "container": '.our-work-slider',
-    "rewind": true,
-    "items": 1,
-    "speed": 600
-  });  
+    "container":  '.our-work-slider',
+    "items":      1,
+    "speed":      600,
+    "rewind":     true,
+    "mouseDrag":  true,
+    "touch":      true,
+    "swipeAngle": false,
+    "arrowKeys":  true
+  });
 }
